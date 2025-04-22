2b4cbd4074a2ec0d521686345b721f3f98118fc6
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-22 22:35:08 +0100

    Changes to be committed: 	modified:   bookapp/src/app/books/books.reduces.ts

--- bookapp/src/app/books/books.reduces.ts
+++ bookapp/src/app/books/books.reduces.ts
@@ -1,2 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
 import { AddBook, RemoveBook } from './book.action';
+import { Book } from '../models/books';
+
+export const initializeState: ReadonlyArray<Book> = [];
+export const bookReducer = createReducer(
+  initializeState,
+  on(AddBook, (state, { id, title, author }) => [
+    ...state,
+    { id, title, author },
+  ])
+);
